9c837cefa702f4987aa4c42705d50a7e69278432
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-08-31 20:21:57 +0000

    solution

--- index.js
+++ index.js
@@ -3,5 +3,9 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-  
+    const count = {}
+    for (i of nums){
+        count[i] ? count[i]++ : count[i]=1
+    }
+    return (Object.keys(count).find(key => count[key] === 1))
 };
